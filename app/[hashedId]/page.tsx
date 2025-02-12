f10f6dc1e8fa37e66699701b526c063790094610
Rahul Sharma <rs3296472t@gmail.com>
2025-02-12 23:46:29 +0530

    hashedu

--- app/[hashedId]/page.tsx
+++ app/[hashedId]/page.tsx
@@ -1,3 +1,5 @@
+
+//@ts-nocheck
 "use client"
 import React, { useEffect, useState } from 'react';
 import { FaCross, FaImage, FaPlus } from "react-icons/fa6";
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,3 +1,5 @@
+
+//@ts-nocheck
 "use client";
 import React, { use, useEffect, useState } from "react";
 import {
